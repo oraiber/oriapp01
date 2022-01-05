4526f0cb74bb88f679148f79c63cf1769f7b17c9
oraiber <ginsan985@gmail.com>
2022-01-05 12:33:27 +0900

    編集ページ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,6 +16,23 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.update(tweet_params)
+      redirect_to root_path
+    else
+      redirect_to edit_tweet_path
+    end
+  end
+
+  def show
+    
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:text).merge(user_id: current_user.id)
