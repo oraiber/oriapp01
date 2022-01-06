1c2bba2b0b64db3b8bb4ec94aad397e20735124c
oraiber <ginsan985@gmail.com>
2022-01-06 20:34:27 +0900

    修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,5 +2,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :text, {presence: true, lengh: {maximum: 500}}
+  validates :text, presence: true, lengh: {maximum: 500}
 end
