63dd941e63fce66779997be00f8bb4c9f3e18770
oraiber <ginsan985@gmail.com>
2022-01-07 10:18:03 +0900

    修正３

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :tweet
   belongs_to :user
 
-  validates :content, presence: true, lengh: {maximum: 140}
+  validates :content, presence: true, lengh: {maximum: 150}
 end
